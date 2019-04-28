76512690031da85f38e2055fbffb768244f41a46
js2653 <50033781+js2653@users.noreply.github.com>
2019-04-28 13:52:04 -0400

    Start Proyect

---
+++ medicalassitent.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace medicalassitent.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ medicalassitent.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace medicalassitent.UIClassic
+{
+    public class Class1
+    {
+    }
+}
